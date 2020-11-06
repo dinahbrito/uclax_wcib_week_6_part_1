777fe7a28c54ca522a67b6899891b42e8c6080a5
Dinah Brito <Dinah@DMBA.local>
2020-11-05 17:40:18 -0800

    darken button put back in

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,20 @@ for(let i = 1; i <= 5; i++) {
     displayedImage.src = e.target.src;
   }
 }
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+
+btn.onclick = function() {
+  const btnClass = btn.getAttribute('class');
+  if(btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
